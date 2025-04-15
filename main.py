fbecd8f735ddd8990408db9a4d0dde138834e755
Sabbir098 <86889163+Sabbir098@users.noreply.github.com>
2025-04-15 13:39:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+
+# Variable and data type :
+
+Name = "Md. Sabbir Hossain"
+Age = 23
+
+iamNowStudent = True
+
+print(type(Name))
+print(type(Age))
+print(type, bool(iamNowStudent))
+
+# Arithmatic Operation :
+
+multiplying = Age * 4
+Division = Age / 3
+Addition = Age + 7
+Subtraction = Age - 3
+
+print(multiplying)
+print(Division)
+print(Addition)
+print(Subtraction)
+
+# Comparison Operator :
+
+Random_age = 18
+
+print(Age > Random_age or Age < Random_age)
+
+x = 99
+y = 91
+
+print(x == y)
+
+a = 26
+b = 12
+
+print(a != b)
+
+x = 5
+y = 3
+
+print(x > y)
+
+x = 33
+y = 23
+
+print(x < y)
+
+# Logical Operator :
+
+notMyAge = 18
+
+print(notMyAge > 3 and notMyAge < 24)
+
+print(notMyAge > 3 or notMyAge < 4)
+
+print(not(notMyAge > 3 and notMyAge < 25))
+
+# Assignment Operator
+
+c = 99
+
+c += 3
+
+print(c)
+
+d = 99
+
+d -= 3
+
+print(d)
+
+z = 99
+
+z *= 3
+
+print(z)
+
+l = 98
+
+l/= 4
+
+print(l)
+
+# Identity Operator :
+
+x = ["Jennie Kim", "Kyle Jenner"]
+y = ["Jennie Kim", "Kyle Jenner"]
+
+z = x
+
+print(x is z)
+
+print(x is y)
+
+print(x == y)
+
+one = ["Jennie Kim", "Kyle Jenner"]
+two = ["Jennie Kim", "Kyle Jenner"]
+
+z = one
+
+print(one is not z)
+
+print(one is not two)
+
+print(one != two)
+
+# Membership Operator :
+
+List = ["Lana Del", "Ariana Grande", "Selena Gomez"]
+
+print("Selena Gomez" in List)
+print("Rihana" not in List)
+
